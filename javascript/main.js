50be327dcbeee143891d547a962f61cc93cb3e0b
jhenderchaustre <jhenderchaustrear@gmail.com>
2023-10-11 18:20:39 -0300

    js final

--- javascript/main.js
+++ javascript/main.js
@@ -5,32 +5,32 @@ const articulos = [
     { nombre: "zapatillas", precio: 25000 },
     { nombre: "buzo", precio: 10500 },
 ];
-let carrito = []
+let carrito = [];
 
 function calcularPrecioConIVA(total) {
-    const iva = 0.21; // 21% de IVA
+    const iva = 0.21;
     const precioConIVA = total + (total * iva);
     return precioConIVA;
 }
 
-let inicio = prompt("Bienvenido, ¿estás listo para comenzar tu compra?")
+let inicio = prompt("Bienvenido, ¿estás listo para comenzar tu compra?");
 
 while (inicio !== "si" && inicio !== "no") {
-    alert("Por favor, indícanos si o no")
-    inicio = prompt("Por favor, indícanos si o no")
+    alert("Por favor, indícanos si o no");
+    inicio = prompt("Por favor, indícanos si o no");
 }
 
 if (inicio == "si") {
-    alert("¡Buena elección! Este es nuestro catálogo de productos")
+    alert("¡Buena elección! Este es nuestro catálogo de productos");
     let productos = articulos.map((articulo) => articulo.nombre + " $" + articulo.precio + " + iva");
     alert(productos.join("\n"));
 } else if (inicio == "no") {
-    alert("Muchas gracias por visitar nuestra tienda en línea, te esperamos pronto!")
+    alert("Muchas gracias por visitar nuestra tienda en línea, te esperamos pronto!");
 }
 
 while (inicio !== "no") {
-    let articulo = prompt("Agrega un artículo al carrito")
-    let precio = 0
+    let articulo = prompt("Agrega un artículo al carrito");
+    let precio = 0;
 
     if (articulo == "remera" || articulo == "anteojos" || articulo == "gorra" || articulo == "zapatillas" || articulo == "buzo") {
         switch (articulo) {
@@ -53,26 +53,25 @@ while (inicio !== "no") {
                 break;
         }
 
-        let unidades = parseInt(prompt("Agrega la cantidad de unidades que deseas al carrito"))
+        let unidades = parseInt(prompt("Agrega la cantidad de unidades que deseas al carrito"));
 
-        carrito.push({ articulo, unidades, precio })
-        console.log(carrito)
+        carrito.push({ articulo, unidades, precio });
+        console.log(carrito);
 
-        inicio = prompt("¿Quieres agregar otro artículo?")
+        inicio = prompt("¿Quieres agregar otro artículo?");
     } else {
-        alert("Disculpa, no tenemos ese artículo")
+        alert("Disculpa, no tenemos ese artículo");
     }
 }
 
 if (inicio === "no") {
-    alert("¡Muchas gracias por tu compra!")
+    alert("Presiona aceptar para continuar");
     carrito.forEach((item) => {
         console.log(`Artículo: ${item.articulo}, Unidades: ${item.unidades}, Precio: $${item.precio}`);
-    })
+    });
 
     const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
     const precioFinalConIVA = calcularPrecioConIVA(total);
-    console.log(El` precio final con IVA es: $${precioFinalConIVA}`)
-    alert(El`Muchas grac : $${precioFinalConIVA}`)
-    
+    console.log(`El precio final con IVA es: $${precioFinalConIVA}`);
+    alert(`Hemos procesado tu compra, el total a pagar es: $${precioFinalConIVA} ¡Muchas gracias por elegirnos!`);
 }
