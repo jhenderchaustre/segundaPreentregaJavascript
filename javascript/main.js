d452f0cf83c4fe0504f0d8238814c4372aa20110
jhenderchaustre <jhenderchaustrear@gmail.com>
2023-10-10 23:18:18 -0300

    js casi terminado

--- javascript/main.js
+++ javascript/main.js
@@ -7,26 +7,32 @@ const articulos = [
 ];
 let carrito = []
 
+function calcularPrecioConIVA(total) {
+    const iva = 0.21; // 21% de IVA
+    const precioConIVA = total + (total * iva);
+    return precioConIVA;
+}
+
 let inicio = prompt("Bienvenido, ¿estás listo para comenzar tu compra?")
 
-while (inicio != "si" && inicio != "no") {
+while (inicio !== "si" && inicio !== "no") {
     alert("Por favor, indícanos si o no")
     inicio = prompt("Por favor, indícanos si o no")
 }
+
 if (inicio == "si") {
     alert("¡Buena elección! Este es nuestro catálogo de productos")
-    let productos = articulos.map((articulo) => articulo.nombre + " " + "$" + articulo.precio + " " + "+ iva");
+    let productos = articulos.map((articulo) => articulo.nombre + " $" + articulo.precio + " + iva");
     alert(productos.join("\n"));
 } else if (inicio == "no") {
-    alert( "Muchas gracias por visitar nuestra tienda online, te esperamos pronto!")
-    console.log(inicio)
+    alert("Muchas gracias por visitar nuestra tienda en línea, te esperamos pronto!")
 }
 
-while(inicio != "no"){
-    let articulo = prompt("Agrega un articulo al carrito")
+while (inicio !== "no") {
+    let articulo = prompt("Agrega un artículo al carrito")
     let precio = 0
 
-    if (articulo == "remera" || articulo == "anteojos" || articulo == "gorra" || articulo == "zapatillas" || articulo == "buzo"){
+    if (articulo == "remera" || articulo == "anteojos" || articulo == "gorra" || articulo == "zapatillas" || articulo == "buzo") {
         switch (articulo) {
             case "remera":
                 precio = 5000;
@@ -44,22 +50,29 @@ while(inicio != "no"){
                 precio = 10500;
                 break;
             default:
-                break;    
+                break;
         }
 
-    let unidades = parseInt(promt("Agregue la cantidad de unidades que quieres aqgregar al carrito"))
+        let unidades = parseInt(prompt("Agrega la cantidad de unidades que deseas al carrito"))
 
-    carrito.push({articulo, unidades, precio})
-    console.log(carrito)
+        carrito.push({ articulo, unidades, precio })
+        console.log(carrito)
+
+        inicio = prompt("¿Quieres agregar otro artículo?")
     } else {
-      alert("Disculpa, no tenemos ese articulo")
+        alert("Disculpa, no tenemos ese artículo")
     }
-    inicio = prompt("Quieres agregar otro articulo?")
+}
 
-    while(inicio ==="no"){
-        alert("Muchas gracias por tu compra!")
-        carrito.forEach((carritoFinal) => {
-            console.log()
-        })
-    }
+if (inicio === "no") {
+    alert("¡Muchas gracias por tu compra!")
+    carrito.forEach((item) => {
+        console.log(`Artículo: ${item.articulo}, Unidades: ${item.unidades}, Precio: $${item.precio}`);
+    })
+
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+    const precioFinalConIVA = calcularPrecioConIVA(total);
+    console.log(El` precio final con IVA es: $${precioFinalConIVA}`)
+    alert(El`Muchas grac : $${precioFinalConIVA}`)
+    
 }
